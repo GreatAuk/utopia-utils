a05c1a69b850951a6a6a1c9f7d299f9ef721e973
Utopia <greatauk11@gmail.com>
2023-01-09 00:31:35 +0800

    refactor: treeFindNode

--- src/treeTools/treeFindNode.ts
+++ src/treeTools/treeFindNode.ts
@@ -1,5 +1,5 @@
-import { DEFAULT_FIELD_NAMES } from './constant'
 import type { FieldNames } from './type'
+import { breadthFirstTraverse } from './breadthFirstTraverse'
 
 interface Options {
   /** Customize node field name */
@@ -28,29 +28,26 @@ interface Options {
  * ```
  */
 export function treeFindNode<TreeNode>(tree: TreeNode[] | TreeNode, predicate: (node: TreeNode) => boolean, options?: Options) {
+  const { isFindAll, fieldNames } = options || {}
   if (!predicate) {
     console.warn('predicate is required')
     return []
   }
-  const { fieldNames, isFindAll } = options || {}
-  const { children } = { ...DEFAULT_FIELD_NAMES, ...fieldNames }
-  const queue = Array.isArray(tree) ? [...tree] : [tree]
+
   const res: TreeNode[] = []
 
-  while (queue.length) {
-    const node = queue.shift()
-    if (!node)
-      continue
-
-    if (predicate(node)) {
-      res.push(node)
-      if (!isFindAll)
-        return res
-    }
-    // @ts-expect-error - This is ok
-    if (node[children])
-    // @ts-expect-error - This is ok
-      queue.push(...node[children])
-  }
+  breadthFirstTraverse<TreeNode>(
+    tree,
+    (node) => {
+      if (predicate(node)) {
+        res.push(node)
+        if (!isFindAll)
+          return false
+      }
+    },
+    {
+      fieldNames,
+    },
+  )
   return res
 }
