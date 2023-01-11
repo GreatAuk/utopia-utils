c0a868ea3be8340704cc718c1993fa661e4a2728
Utopia <greatauk11@gmail.com>
2023-01-11 22:29:59 +0800

    feat: remove util - parseDuration

--- src/utils.ts
+++ src/utils.ts
@@ -6,25 +6,3 @@
 export const toTypeString = (value: unknown): string => {
   return Object.prototype.toString.call(value)
 }
-
-export type Duration = number | `${number}s` | `${number}ms`
-/**
- * "If the duration is a number, return it, otherwise if it's a string, parse it and return the number
- * of milliseconds."
- * @param {Duration} duration - The duration to be converted to milliseconds.
- * @returns The number of milliseconds.
- */
-export function parseDuration(d: Duration) {
-  if (typeof d === 'number') {
-    if (isNaN(d) || d < 0)
-      throw new Error(`Invalid duration: "${d}".`)
-    return d
-  }
-  else if (/\d+s/.test(d)) {
-    return +d.slice(0, -1) * 1000
-  }
-  else if (/\d+ms/.test(d)) {
-    return +d.slice(0, -2)
-  }
-  throw new Error(`Unknown duration: "${d}".`)
-}
