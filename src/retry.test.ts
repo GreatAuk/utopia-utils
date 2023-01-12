b2abae8cd5a65894a80f92f1a9f677cc414ecb54
Utopia <greatauk11@gmail.com>
2023-01-12 23:44:01 +0800

    feat: new function - retry

---
+++ src/retry.test.ts
@@ -0,0 +1,102 @@
+import { describe, expect, it } from 'vitest'
+import { retry } from './retry'
+import { sleep } from './sleep'
+
+describe('retry', async () => {
+  it('throws if retryTime is not a positive number', async () => {
+    await expect(() => retry(() => {}, -1)).rejects.toThrowError()
+  })
+
+  it('should return the result of the function', async () => {
+    const fn = () => Promise.resolve('foo')
+    const [err, res] = await retry(fn, 1)
+    expect(err).toBe(null)
+    expect(res).toBe('foo')
+  })
+
+  it('should retry the function', async () => {
+    let attempt = 0
+    const fn = () => {
+      attempt++
+      return Promise.resolve('foo')
+    }
+    const [err, res] = await retry(fn, 2)
+    expect(err).toBe(null)
+    expect(res).toBe('foo')
+    expect(attempt).toBe(1)
+  })
+
+  it('should retry the function in retryTime', async () => {
+    let callNum = 0
+    const fn = () => {
+      callNum++
+      return Promise.reject(new Error('foo'))
+    }
+    await retry(fn, 2)
+    expect(callNum).toBe(3)
+
+    let callNum2 = 0
+    const fn2 = () => {
+      callNum2++
+      return Promise.reject(new Error('foo'))
+    }
+    await retry(fn2, 0)
+    expect(callNum2).toBe(1)
+  })
+
+  it('should retry the function with delay (number)', async () => {
+    let callNum = 0
+    const fn = () => {
+      callNum++
+      return Promise.reject(new Error('foo'))
+    }
+    const startTime = Date.now()
+    const [err, res] = await retry(fn, 2, 10)
+    const endTime = Date.now()
+    expect(err).toBeInstanceOf(Error)
+    expect(res).toBe(null)
+    expect(callNum).toBe(3)
+    expect(endTime - startTime).toBeGreaterThanOrEqual(20)
+  })
+  it('should retry the function with delay (function return number)', async () => {
+    let callNum = 0
+    const fn = () => {
+      callNum++
+      return Promise.reject(new Error('foo'))
+    }
+    const startTime = Date.now()
+    const [err, res] = await retry(fn, 2, (attemptTime) => {
+      return attemptTime * 5
+    })
+    const endTime = Date.now()
+    expect(err).toBeInstanceOf(Error)
+    expect(res).toBe(null)
+    expect(callNum).toBe(3)
+    // 1 * 5 + 2 * 5
+    expect(endTime - startTime).toBeGreaterThanOrEqual(15)
+  })
+  it('should retry the function with delay (function return promise)', async () => {
+    let callNum = 0
+    const fn = () => {
+      callNum++
+      return Promise.reject(new Error('foo'))
+    }
+    const startTime = Date.now()
+    const [err, res] = await retry(fn, 2, async () => {
+      return await sleep(5)
+    })
+    const endTime = Date.now()
+    expect(err).toBeInstanceOf(Error)
+    expect(res).toBe(null)
+    expect(callNum).toBe(3)
+    expect(endTime - startTime).toBeGreaterThanOrEqual(10)
+  })
+  it('should retry normal function', async () => {
+    const sum = (a: number, b: number) => {
+      return a + b
+    }
+    const [err, res] = await retry(() => sum(2, 10), 2)
+    expect(err).toBe(null)
+    expect(res).toBe(12)
+  })
+})
---
+++ src/retry.ts
@@ -0,0 +1,57 @@
+import { sleep } from './sleep'
+import { isFunction, isNumber } from './is'
+
+type RetryDelay = number | ((attemptTime: number) => Promise<void>) | ((attemptTime: number) => number)
+
+/**
+ * It retries a function until it succeeds or the retry time is exceeded
+ * @param {(() => Promise<T>) | (() => T)} fn - The function to retry.
+ * @param {number} retryTime - The number of times to retry the function.
+ * @param {RetryDelay} [delay] - The delay between each retry. Can be a number or a function that
+ * return a milliseconds or function return a promise.
+ * @returns A tuple of type [E, null] | [null, T]
+ * @example
+ * ```
+    let callNum = 0
+    const fn = () => {
+      callNum++
+      return Promise.reject(new Error('foo'))
+    }
+    const [err, res] = await retry(fn, 2, (attemptTime) => {
+      return attemptTime * 5
+    })
+    // output
+    // err is Error, res is null, callNum is 3
+    // execute time is greater than or equal to 15
+ * ```
+ */
+export async function retry<T, E = Error>(fn: (() => Promise<T>) | (() => T), retryTime: number, delay?: RetryDelay): Promise<[E, null] | [null, T]> {
+  if (!isNumber(retryTime) || retryTime < 0)
+    throw new Error('retryTime must be a positive number')
+
+  let attemptTime = 0
+  let res: T
+  while (attemptTime <= retryTime) {
+    try {
+      res = await fn()
+      break
+    }
+    catch (err) {
+      attemptTime++
+
+      if (attemptTime > retryTime)
+        return [err, null] as [E, null]
+
+      if (isNumber(delay)) {
+        await sleep(delay)
+      }
+      else if (isFunction(delay)) {
+        const tempRes = await delay(attemptTime)
+        if (isNumber(tempRes))
+          await sleep(tempRes)
+      }
+    }
+  }
+  // @ts-expect-error - TS doesn't know that res is defined here
+  return [null, res]
+}
