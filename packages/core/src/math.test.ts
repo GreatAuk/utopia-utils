cc2665637d422670be04fa7887a0f06c80576480
Utopia <greatauk11@gmail.com>
2023-02-05 03:12:30 +0800

    feat: new function: average

--- packages/core/src/math.test.ts
+++ packages/core/src/math.test.ts
@@ -1,11 +1,16 @@
 import { describe, expect, it } from 'vitest'
 
-import { toFixed } from './math'
+import { average, toFixed } from './math'
 
-describe('toFixed', () => {
-  it('should work', () => {
+describe('math functions', () => {
+  it('toFixed', () => {
     expect(toFixed(0.615, 2)).toBe(0.61)
     expect(toFixed(10, 2)).toBe(10)
     expect(toFixed(1.005, 2)).toBe(1)
+  })
+  it('average', () => {
+    expect(average(1, 2, 3)).toBe(2)
+    expect(average(...[1, 2, 3])).toBe(2)
+    expect(average(...[])).toBe(NaN)
   })
 })
--- packages/core/src/math.ts
+++ packages/core/src/math.ts
@@ -7,3 +7,15 @@
 export function toFixed(num: number, precision: number): number {
   return Number(num.toFixed(precision))
 }
+
+/**
+ * get the average of all the numbers passed in
+ * @example
+ * ```
+ * average(1, 2, 3) // 2
+ * average(...[1, 2, 3]) // 2
+ * ```
+ */
+export function average(...args: number[]): number {
+  return args.reduce((a, b) => a + b, 0) / args.length
+}
