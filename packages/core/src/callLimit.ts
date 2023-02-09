53125b21311fb76f80292d38b7d585aeb4e21cf4
Utopia <greatauk11@gmail.com>
2023-02-09 12:02:52 +0800

    feat: callLimit - optimize type define

--- packages/core/src/callLimit.ts
+++ packages/core/src/callLimit.ts
@@ -13,15 +13,13 @@ export function callLimit<T extends (...args: any[]) => any>(fn: T, limit = 1) {
   if (!isNumber(limit))
     throw new TypeError('limit expected a number')
 
-  let res: any
-  return function (this: any, ...args: any[]) {
+  let res: ReturnType<T> | undefined
+  return function (this: ThisParameterType<T>, ...args: Parameters<T>) {
     if (--limit >= 0)
       res = fn.apply(this, args)
     if (limit < 0)
       res = undefined
 
     return res
-  } as AddReturnType<T, undefined>
+  }
 }
-
-type AddReturnType<T extends (...args: any[]) => any, A> = T extends (...args: infer P) => infer R ? (...args: P) => R | A : T
