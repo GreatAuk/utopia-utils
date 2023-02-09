5ebe004451212c47c7baeb3c00040709bcaf65ab
Utopia <greatauk11@gmail.com>
2023-02-09 18:06:08 +0800

    feat:  callLimit-Beyond the limit case
    if call times more than limit, return latest called value

--- packages/core/src/callLimit.test.ts
+++ packages/core/src/callLimit.test.ts
@@ -9,12 +9,15 @@ describe('callLimit', () => {
     fn1_()
     fn1_()
     expect(fn1).toHaveBeenCalledTimes(2)
-
+  })
+  it('if call times more than limit, return latest called value', () => {
     const add = (a: number, b: number) => a + b
     const add_ = callLimit(add, 1)
     const res = add_(1, 1)
-    const res2 = add_(1, 1)
-    expect(res).toMatchInlineSnapshot('2')
-    expect(res2).toMatchInlineSnapshot('undefined')
+    const res2 = add_(1, 2)
+    const res3 = add_(1, 3)
+    expect(res).toEqual(2)
+    expect(res2).toEqual(2)
+    expect(res3).toEqual(2)
   })
 })
--- packages/core/src/callLimit.ts
+++ packages/core/src/callLimit.ts
@@ -1,10 +1,21 @@
 import { isFunction, isNumber } from '@utopia-utils/share'
 
 /**
- * returns a function that calls the original function the specified number of times
+ * returns a function that can call the original function the specified number of times.
+ *
+ * if call times more than limit, return latest called value
  * @param {T} fn - The function to call
  * @param [limit=1] - The number of times the function can be called.
- * @returns
+ * @returns {Function} new function that calls the original function the specified number of times
+ * @example
+ * ```ts
+    const add = (a: number, b: number) => a + b
+    const addLimit = callLimit(add, 1)
+    const res = addLimit(1, 1)
+    const res2 = addLimit(1, 2)
+    expect(res).toEqual(2)
+    expect(res2).toEqual(2) // if call times more than limit, return latest called value
+ * ```
  */
 export function callLimit<T extends (...args: any[]) => any>(fn: T, limit = 1) {
   if (!isFunction(fn))
@@ -13,13 +24,12 @@ export function callLimit<T extends (...args: any[]) => any>(fn: T, limit = 1) {
   if (!isNumber(limit))
     throw new TypeError('limit expected a number')
 
-  let res: ReturnType<T> | undefined
+  let cachedValue: ReturnType<T>
   return function (this: ThisParameterType<T>, ...args: Parameters<T>) {
     if (--limit >= 0)
-      res = fn.apply(this, args)
-    if (limit < 0)
-      res = undefined
+      cachedValue = fn.apply(this, args)
+    // if limit < 0, can not call fn again
 
-    return res
+    return cachedValue
   }
 }
