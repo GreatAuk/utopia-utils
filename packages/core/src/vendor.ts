9f0e7561235434131a31ba9e1f5f15ecbf3fbb7e
Utopia <greatauk11@gmail.com>
2023-02-11 16:24:07 +0800

    chore: bundle vendor package with type definition

--- packages/core/src/vendor.ts
+++ packages/core/src/vendor.ts
@@ -1,3 +1,3 @@
 export { debounce, throttle } from 'throttle-debounce'
 export { default as mitt } from 'mitt'
-export { FileSaverOptions, saveAs } from 'file-saver'
+// export { saveAs } from 'file-saver'
