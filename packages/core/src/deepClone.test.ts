ffba2282b1b169010c17771efa97828349aebf23
Utopia <greatauk11@gmail.com>
2023-02-12 01:50:05 +0800

    feat: new function - deepClone

---
+++ packages/core/src/deepClone.test.ts
@@ -0,0 +1,180 @@
+import { deepClone } from './deepClone'
+
+describe('deepClone', () => {
+  it('Returns equal data for Null/undefined/functions/etc', () => {
+    // Null
+    expect(deepClone(null)).toBeNull()
+
+    // Undefined
+    expect(deepClone(undefined)).toBeUndefined()
+
+    // Function
+    const func = () => {}
+    expect(deepClone(func)).toBe(func)
+
+    // Etc: numbers and string
+    expect(deepClone(5)).toBe(5)
+    expect(deepClone('string')).toBe('string')
+    expect(deepClone(false)).toBe(false)
+    expect(deepClone(true)).toBe(true)
+  })
+  it('Returns equal data for Date', () => {
+    const date = '2012-01-26T13:51:50.417Z'
+    expect(deepClone(new Date(date))).toEqual(new Date(date))
+  })
+
+  it('Returns equal data for RegExp', () => {
+    const regexp = /^$/
+    expect(deepClone(regexp)).toEqual(regexp)
+  })
+
+  it('Returns equal data for Arrays', () => {
+    const tests = [
+      [5, 5, 8, 'string'], // Flat
+      [5, 5, 8, { a: 'string' }, [7, 9]], // Attached
+    ]
+
+    tests.forEach((src) => {
+      const copy = deepClone(src)
+
+      expect(src).toEqual(copy)
+    })
+  })
+
+  it('Returns equal data for Object', () => {
+    const src = {
+      a: 5,
+      b: 6,
+    }
+
+    const copy = deepClone(src)
+
+    expect(src).toEqual(copy)
+  })
+
+  it('Returns equal data for Map', () => {
+    const src = new Map([['foo', 'bar']])
+
+    const copy = deepClone(src)
+
+    expect(src).toEqual(copy)
+  })
+
+  it('Returns equal data for Set', () => {
+    const src = new Set(['foo', 'bar'])
+
+    const copy = deepClone(src)
+
+    expect(src).toEqual(copy)
+  })
+
+  test('Doesn\'t clone function', () => {
+    const src = function b() {}
+
+    const copy = deepClone(src)
+
+    expect(copy).toBe(src)
+  })
+
+  test('Clones Date object', () => {
+    const src = new Date()
+
+    const copy = deepClone(src)
+
+    copy.setHours(src.getHours() + 1) // +1 hour
+
+    expect(copy.getHours()).not.toBe(src.getHours())
+  })
+
+  test('Clones RegExp', () => {
+    const src = /\d/g
+
+    const copy = deepClone(src)
+
+    expect(copy).not.toBe(src)
+  })
+
+  test('Clones Array with nested data', () => {
+    const src = [1, 'hello', [null, 'lalka']]
+
+    let copy = deepClone(src)
+
+    // @ts-expect-error for test
+    copy[2][0] = 'mutated'
+    // @ts-expect-error for test
+    expect(src[2][0]).toBeNull()
+
+    copy = copy.map(_ => 'mutated')
+
+    expect(src.every(i => i !== 'mutated')).toBeTruthy()
+  })
+
+  test('Clones nested Arrays', () => {
+    // @ts-expect-error for test
+    const src = []
+    // @ts-expect-error for test
+    src.push(src, 2, src, 3)
+
+    const copy = deepClone(src)
+    expect(copy[0]).toEqual(copy)
+    expect(src[0]).toEqual(src)
+    expect(copy[0]).not.toBe(src[0])
+  })
+
+  test('Clones nested Objects', () => {
+    const src = { a: 1, b: { c: 1, d: [1, 2, 3] } }
+    const srcValues = { a: 1, b: { c: 1, d: [1, 2, 3] } }
+
+    const copy = deepClone(src)
+    copy.a = 2
+    // @ts-expect-error for test
+    copy.b.c = 'asdf'
+    copy.b.d[1] = 4
+    expect(src).toEqual(srcValues)
+  })
+
+  it('clones circular data', () => {
+    const a = { foo: 'bar' }
+    // @ts-expect-error for test
+    a.baz = a
+    const b = { foo: 'bar' }
+    // @ts-expect-error for test
+    b.baz = b
+    expect(deepClone(a)).toEqual(b)
+  })
+
+  it('Clones Map', () => {
+    const src = new Map([['foo', 'bar']])
+
+    const copy = deepClone(src)
+
+    copy.set('foo', 'baz')
+
+    expect(src.get('foo')).toEqual('bar')
+  })
+
+  it('Clones Set', () => {
+    const src = new Set(['foo', 'bar'])
+
+    const copy = deepClone(src)
+
+    copy.add('baz')
+
+    expect(src.has('baz')).toBeFalsy()
+  })
+
+  it('circular deps', () => {
+    const src = {
+      a: 1,
+      test: new Map(),
+    }
+    src.test.set('key', src)
+
+    const copy = deepClone(src)
+
+    copy.a = 2
+
+    expect(src.a === copy.a).toBeFalsy()
+    expect(src.test.get('key') === copy.test.get('key')).toBeFalsy()
+  })
+})
---
+++ packages/core/src/deepClone.ts
@@ -0,0 +1,78 @@
+function clone<T>(src: T, seen = new Map()) {
+  // Immutable things - null, undefined, functions, symbols, etc.
+  if (!src || typeof src !== 'object')
+    return src
+
+  // Things we've seen already (circular refs)
+  if (seen.has(src))
+    return seen.get(src)
+
+  // Basic pattern for cloning something below here is:
+  // 1. Create copy
+  // 2. Add it to `seen` immediately, so we recognize it if we see it in
+  //    subordinate members
+  // 3. clone subordinate members
+
+  let copy
+  if (isDom(src)) {
+    // DOM Node
+    copy = src.cloneNode(true)
+    seen.set(src, copy)
+  }
+  else if (src instanceof Date) {
+    // Date
+    copy = new Date(src.getTime())
+    seen.set(src, copy)
+  }
+  else if (src instanceof RegExp) {
+    // RegExp
+    copy = new RegExp(src)
+    seen.set(src, copy)
+  }
+  else if (Array.isArray(src)) {
+    // Array
+    copy = new Array(src.length)
+    seen.set(src, copy)
+    for (let i = 0; i < src.length; i++) copy[i] = clone(src[i], seen)
+  }
+  else if (src instanceof Map) {
+    // Map
+    copy = new Map()
+    seen.set(src, copy)
+    for (const [k, v] of src.entries()) copy.set(k, clone(v, seen))
+  }
+  else if (src instanceof Set) {
+    // Set
+    copy = new Set()
+    seen.set(src, copy)
+    for (const v of src) copy.add(clone(v, seen))
+  }
+  else if (src instanceof Object) {
+    // Object
+    copy = {} as any
+    seen.set(src, copy)
+    for (const [k, v] of Object.entries(src)) copy[k] = clone(v, seen)
+  }
+  else {
+    // Unrecognized thing.  It's better to throw here than to return `src`, as
+    // we don't know whether src needs to be deep-copied here.
+    throw new TypeError(`Unable to clone ${src}`)
+  }
+
+  return copy
+}
+
+/**
+ * It takes a source object and returns a deep clone of it
+ *
+ * fork from nanoclone, and add type definition. https://github.com/Kelin2025/nanoclone
+ * @param {T} src - The object to be cloned.
+ * @returns A new object with the same properties as the original object.
+ */
+export function deepClone<T>(src: T): T {
+  return clone(src, new Map())
+}
+
+function isDom(obj: any): obj is Node {
+  return obj.nodeType && 'cloneNode' in obj
+}
