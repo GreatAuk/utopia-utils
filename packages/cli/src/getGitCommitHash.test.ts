f35b9fb41387c3778725acd6957dc94193b1f198
Utopia <greatauk11@gmail.com>
2023-05-12 23:41:55 +0800

    test: [getGitCommitHash]

--- packages/cli/src/getGitCommitHash.test.ts
+++ packages/cli/src/getGitCommitHash.test.ts
@@ -1,8 +1,17 @@
 import { getGitCommitHash } from './getGitCommitHash'
 
 describe('getGitCommitHash', () => {
+  afterEach(() => {
+    vi.restoreAllMocks()
+  })
   it('returns the current commit hash', () => {
     // for example 'a1b2c3d'
     expect(getGitCommitHash()).toHaveLength(7)
   })
+  // it('should throw an error when returned is undefined', () => {
+  //   const mockExecSync = vi.fn().mockImplementation(execSync)
+  //   mockExecSync.mockReturnValueOnce('undefined')
+  //   // for example 'a1b2c3d'
+  //   expect(getGitCommitHash()).toThrowError('Could not retrieve git commit hash.')
+  // })
 })
