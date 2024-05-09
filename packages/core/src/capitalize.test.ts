13af4adb9c492cde7e5bc8ba2677a25c91c5534e
Utopia <greatauk11@gmail.com>
2024-05-09 17:59:19 +0800

    feat: improve function arrLast

--- packages/core/src/capitalize.test.ts
+++ packages/core/src/capitalize.test.ts
@@ -3,5 +3,7 @@ import { capitalize } from './capitalize'
 describe('capitalize', () => {
   it('should capitalize the first letter of a string', () => {
     expect(capitalize('foo')).toBe('Foo')
+    expect(capitalize('hello world')).toBe('Hello world')
+    expect(capitalize('')).toBe('')
   })
 })
