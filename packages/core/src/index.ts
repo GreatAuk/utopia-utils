b23615bcaca49e026c4c646d31ca69369826aef5
Utopia <greatauk11@gmail.com>
2024-05-24 10:52:20 +0800

    feat: bump js-cookie 3.0.2 -> 3.0.5

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -35,7 +35,7 @@ export * from './pipe'
 export * from './randomInt'
 export * from './randomString'
 export * from './retry'
-export * from './sleep'mch
+export * from './sleep'
 export * from './sort'
 export * from './stringFormatter'
 export * from './union'
