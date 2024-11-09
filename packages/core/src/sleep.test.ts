e6becc4426881253d25bce6c28a653b58767ee3c
utopia <greatauk11@gmail.com>
2024-11-09 22:25:49 +0800

    test: [sleep] update test

--- packages/core/src/sleep.test.ts
+++ packages/core/src/sleep.test.ts
@@ -1,10 +1,24 @@
 import { sleep } from './sleep'
 
 describe('sleep', () => {
+  beforeEach(() => {
+    vi.useFakeTimers()
+  })
+
+  afterEach(() => {
+    vi.restoreAllMocks()
+  })
   it('should sleep for the specified duration', async () => {
+    const ms = 90 * 1000
     const startTime = Date.now()
-    await sleep(10)
-    const endTime = Date.now()
-    expect(endTime - startTime).toBeGreaterThanOrEqual(9)
+    let endTime = 0
+
+    sleep(ms).then(() => {
+      endTime = Date.now()
+    })
+
+    await vi.runAllTimersAsync()
+
+    expect(endTime - startTime).toBeGreaterThanOrEqual(ms)
   })
 })
