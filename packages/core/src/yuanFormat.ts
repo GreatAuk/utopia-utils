0e97bd61be241da9617110305cd9f5d2e2afffd5
Utopia <greatauk11@gmail.com>
2025-08-06 10:30:55 +0800

    docs: [yuanFormat] 更新函数的参数注释

--- packages/core/src/yuanFormat.ts
+++ packages/core/src/yuanFormat.ts
@@ -24,18 +24,22 @@ interface Options {
  * 金额格式化￥ 分转元(如果单位是分)，并保留两位小数，最后添加千位分隔符
  * @param num 金额
  * @param options 格式化选项
- * @param options.unit 单位，分或元，如果单位是分，则会将金额转换为元，默认为 'fen'
- * @param options.prefix 前缀，如果是 true 则使用 ¥，或者使用传入的字符串，默认为 undefined
- * @param options.space 是否在前缀和数值之间添加空格，默认为 false
+ * @param options.unit 单位，分或元，如果单位是分，则会将金额转换为元
+ * @default 'fen'
+ * @param options.prefix 前缀，如果是 true 则使用 ¥，或者使用传入的字符串
+ * @default undefined
+ * @param options.space 是否在前缀和数值之间添加空格
+ * @default undefined
  * @returns 格式化后的金额, 如果输入不是数字，则返回 '-'
  * @example
  * ```ts
  * yuanFormat(100) // '1.00'
  * yuanFormat(100, { unit: 'yuan' }) // '100.00'
  * yuanFormat(100, { prefix: true }) // '¥1.00'
- * yuanFormat(100, { prefix: '$' }) // '$1.00'
+ * yuanFormat(100, { prefix: '元' }) // '元1.00'
  * yuanFormat(100, { prefix: true, space: true }) // '¥ 1.00'
- * yuanFormat(100000, { prefix: 'USD', space: true }) // 'USD 1,000.00'
+ * yuanFormat(100000, { prefix: 'CNY', space: true }) // 'CNY 1,000.00'
+ * yuanFormat(100000, { prefix: '元', space: true }) // '元 1,000.00'
  * yuanFormat('not number') // '-'
  * ```
  */
